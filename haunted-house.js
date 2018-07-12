1eb3da58e19638a978798569dd2d70b08afc48ee
Student <student@Admins-MacBook-Air.local>
2018-07-12 10:59:36 -0700

    uodate

--- haunted-house.js
+++ haunted-house.js
@@ -4,15 +4,15 @@ let readline = require("readline-sync");
 console.log();
 console.log("*****************************************************************");
 console.log("                          HAUNTED HOUSE");
-console.log("                 A game by FirstName LastName");
+console.log("                 A game by Carmen Franco");
 console.log("*****************************************************************");
 console.log("You are standing in front of a spooky haunted house.");
 console.log("You hear what sounds like screaming coming from inside the house.");
 let enterHouse = readline.question("Will you enter the house? (yes or no) ");
 if(enterHouse === "y" || enterHouse === "yes") {
-  let playDoge = readline.question(" You see a doge. Will you play with it or leave the house?");
-  if(playDoge === "y" || playDoge === "yes") {
-    let bffDoge = readline.question(" You guys become BFFs! Would you like to take it home?");
+  let playDoge = readline.question(" You see a doge. Will you play with it or leave the house? (play or leave)");
+  if(playDoge === "p" || playDoge === "play") {
+    let bffDoge = readline.question(" You guys become BFFs! Would you like to take it home? (yes or no)");
     if(bffDoge === "y" || bffDoge === "yes"){
     }
   }
@@ -21,3 +21,23 @@ if(enterHouse === "y" || enterHouse === "yes") {
 
 }
 console.log("Thanks for playing!");
+console.log(`
+                  ▄              ▄
+                  ▌▒█           ▄▀▒▌
+                  ▌▒▒█        ▄▀▒▒▒▐
+                 ▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+               ▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+             ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+            ▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌
+            ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+           ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌
+           ▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌
+          ▌▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐
+          ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+          ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐
+           ▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌
+           ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐
+            ▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌
+              ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀
+                ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀
+                   ▒▒▒▒▒▒▒▒▒▒▀▀`);
