7280ae57c65fcfc1ebe386980d4637bca5ddd354
Student <student@Admins-MacBook-Air.local>
2018-07-11 17:01:53 -0700

    update

--- haunted-house.js
+++ haunted-house.js
@@ -1,5 +1,5 @@
 // Author: FirstName LastName
-var readline = require("readline-sync");
+let readline = require("readline-sync");
 
 console.log();
 console.log("*****************************************************************");
@@ -8,12 +8,16 @@ console.log("                 A game by FirstName LastName");
 console.log("*****************************************************************");
 console.log("You are standing in front of a spooky haunted house.");
 console.log("You hear what sounds like screaming coming from inside the house.");
-var enterHouse = readline.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
-  // your code here
-
+let enterHouse = readline.question("Will you enter the house? (yes or no) ");
+if(enterHouse === "y" || enterHouse === "yes") {
+  let playDoge = readline.question(" You see a doge. Will you play with it or leave the house?");
+  if(playDoge === "y" || playDoge === "yes") {
+    let bffDoge = readline.question(" You guys become BFFs! Would you like to take it home?");
+    if(bffDoge === "y" || bffDoge === "yes"){
+    }
+  }
 } else {
-  // your code here
+  console.log("OK.");
 
 }
 console.log("Thanks for playing!");
